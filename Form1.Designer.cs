845a8beb8aaa9438555e61d3ea152efb977abf3f
EricC <al23760307@ite.edu.mx>
2025-09-16 18:03:38 -0700

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Compilador
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        //VARIABLES GLOBALES
+        public string archivo;
+        public string archivoback;
+        public string archivotrad;
+        public int i_caracter;
+        public int N_caracter;
+        public int N_linea;
+        public char c_caracter;
+        public StreamWriter Escribir;
+        public StreamReader Leer;
+        public int N_error;
+        
+        public int prueba = 0;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            aRCHIVOToolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem1 = new ToolStripMenuItem();
+            nuevoToolStripMenuItem = new ToolStripMenuItem();
+            gUAToolStripMenuItem = new ToolStripMenuItem();
+            gUAToolStripMenuItem1 = new ToolStripMenuItem();
+            toolStripSeparator1 = new ToolStripSeparator();
+            salirToolStripMenuItem1 = new ToolStripMenuItem();
+            cOMPILARToolStripMenuItem = new ToolStripMenuItem();
+            aNALIZARToolStripMenuItem = new ToolStripMenuItem();
+            richTextBox1 = new RichTextBox();
+            Rtbx_salida = new TextBox();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { aRCHIVOToolStripMenuItem, cOMPILARToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.RightToLeft = RightToLeft.No;
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // aRCHIVOToolStripMenuItem
+            // 
+            aRCHIVOToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { toolStripMenuItem1, nuevoToolStripMenuItem, gUAToolStripMenuItem, gUAToolStripMenuItem1, toolStripSeparator1, salirToolStripMenuItem1 });
+            aRCHIVOToolStripMenuItem.Name = "aRCHIVOToolStripMenuItem";
+            aRCHIVOToolStripMenuItem.Size = new Size(70, 20);
+            aRCHIVOToolStripMenuItem.Text = "ARCHIVO";
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(150, 22);
+            toolStripMenuItem1.Text = "Nuevo";
+            toolStripMenuItem1.Click += toolStripMenuItem1_Click;
+            // 
+            // nuevoToolStripMenuItem
+            // 
+            nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
+            nuevoToolStripMenuItem.Size = new Size(150, 22);
+            nuevoToolStripMenuItem.Text = "Abrir";
+            nuevoToolStripMenuItem.Click += AbrirToolStripMenuItem_Click;
+            // 
+            // gUAToolStripMenuItem
+            // 
+            gUAToolStripMenuItem.Name = "gUAToolStripMenuItem";
+            gUAToolStripMenuItem.Size = new Size(150, 22);
+            gUAToolStripMenuItem.Text = "Guardar";
+            gUAToolStripMenuItem.Click += gUAToolStripMenuItem_Click;
+            // 
+            // gUAToolStripMenuItem1
+            // 
+            gUAToolStripMenuItem1.Name = "gUAToolStripMenuItem1";
+            gUAToolStripMenuItem1.Size = new Size(150, 22);
+            gUAToolStripMenuItem1.Text = "Guardar como";
+            gUAToolStripMenuItem1.Click += GuardarComoToolStripMenuItem1_Click;
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(147, 6);
+            // 
+            // salirToolStripMenuItem1
+            // 
+            salirToolStripMenuItem1.Name = "salirToolStripMenuItem1";
+            salirToolStripMenuItem1.Size = new Size(150, 22);
+            salirToolStripMenuItem1.Text = "Salir";
+            salirToolStripMenuItem1.Click += salirToolStripMenuItem1_Click;
+            // 
+            // cOMPILARToolStripMenuItem
+            // 
+            cOMPILARToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { aNALIZARToolStripMenuItem });
+            cOMPILARToolStripMenuItem.Name = "cOMPILARToolStripMenuItem";
+            cOMPILARToolStripMenuItem.Size = new Size(78, 20);
+            cOMPILARToolStripMenuItem.Text = "COMPILAR";
+            cOMPILARToolStripMenuItem.Click += cOMPILARToolStripMenuItem_Click;
+            // 
+            // aNALIZARToolStripMenuItem
+            // 
+            aNALIZARToolStripMenuItem.Name = "aNALIZARToolStripMenuItem";
+            aNALIZARToolStripMenuItem.Size = new Size(130, 22);
+            aNALIZARToolStripMenuItem.Text = "ANALIZAR";
+            aNALIZARToolStripMenuItem.Click += aNALIZARToolStripMenuItem_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Dock = DockStyle.Fill;
+            richTextBox1.Location = new Point(0, 24);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(800, 426);
+            richTextBox1.TabIndex = 1;
+            richTextBox1.Text = "";
+            richTextBox1.TextChanged += richTextBox1_TextChanged;
+            // 
+            // Rtbx_salida
+            // 
+            Rtbx_salida.BackColor = SystemColors.GrayText;
+            Rtbx_salida.Dock = DockStyle.Bottom;
+            Rtbx_salida.Location = new Point(0, 383);
+            Rtbx_salida.Multiline = true;
+            Rtbx_salida.Name = "Rtbx_salida";
+            Rtbx_salida.Size = new Size(800, 67);
+            Rtbx_salida.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(Rtbx_salida);
+            Controls.Add(richTextBox1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem aRCHIVOToolStripMenuItem;
+        private ToolStripMenuItem nuevoToolStripMenuItem;
+        private RichTextBox richTextBox1;
+        private ToolStripMenuItem gUAToolStripMenuItem;
+        private ToolStripMenuItem gUAToolStripMenuItem1;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripMenuItem salirToolStripMenuItem1;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private ToolStripMenuItem cOMPILARToolStripMenuItem;
+        private ToolStripMenuItem aNALIZARToolStripMenuItem;
+        private TextBox Rtbx_salida;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,353 @@
+using System.IO;
+using System.Runtime.Intrinsics.X86;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+namespace Compilador
+{
+
+    /*CONCEPTOS A ENTENDER 
+
+     .Leer.Read() lee uno por uno en ASCII
+
+     */
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            cOMPILARToolStripMenuItem.Enabled = false;
+            //Se empieza con la opcion de "Compilar" DESACTIVADO
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            cOMPILARToolStripMenuItem.Enabled = true;
+            // habilita la opción compilar cuando se realiza un cambio en el texto.
+        }
+
+
+        //***********OPCIONES DEL MENU "ARCHIVO"***********
+
+        //NUEVO
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            archivo = null;
+        }
+
+        //ABRIR
+        private void AbrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog VentanaAbrir = new OpenFileDialog();
+            VentanaAbrir.Filter = "Texto|*.c";
+            if (VentanaAbrir.ShowDialog() == DialogResult.OK)
+            {
+                archivo = VentanaAbrir.FileName;
+                using (StreamReader Leer = new StreamReader(archivo))
+                {
+                    richTextBox1.Text = Leer.ReadToEnd();
+                }
+
+                Form1.ActiveForm.Text = "Compilador - " + archivo;
+                cOMPILARToolStripMenuItem.Enabled = true;
+                //deja compilar hasta que haya un archivo C
+            }
+        }
+
+        //GUARDAR
+        private void gUAToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            guardar();
+        }
+
+        //FUNCIONAMIENTO DE GUARDAR
+        private void guardar()
+        {
+            SaveFileDialog VentanaGuardar = new SaveFileDialog();
+            VentanaGuardar.Filter = "Texto|*.c";
+            if (archivo != null)
+            {
+                using (StreamWriter Escribir = new StreamWriter(archivo))
+                {
+                    Escribir.Write(richTextBox1.Text);
+                }
+            }
+
+            else
+            {
+                if (VentanaGuardar.ShowDialog() == DialogResult.OK)
+                {
+                    archivo = VentanaGuardar.FileName;
+                    using (StreamWriter Escribir = new StreamWriter(archivo))
+                    {
+                        Escribir.Write(richTextBox1.Text);
+                    }
+                }
+            }
+            Form1.ActiveForm.Text = "Compilador - " + archivo;
+        }
+
+        //GUADAR COMO
+        private void GuardarComoToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog VentanaGuardar = new SaveFileDialog();
+            if (VentanaGuardar.ShowDialog() == DialogResult.OK)
+            {
+                archivo = VentanaGuardar.FileName;
+                using (StreamWriter Escribir = new StreamWriter(archivo))
+                {
+                    Escribir.Write(richTextBox1.Text);
+                }
+            }
+        }
+
+        //SALIR
+        private void salirToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //**********FIN DE OPCIONES DEL MENU "ARCHIVO"**********
+
+
+        //**********OPCION DE ANALIZAR **********
+
+        private void aNALIZARToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            guardar();
+            N_error = 0;
+            N_linea = 1;
+            Rtbx_salida.Clear();
+            archivoback = archivo.Remove(archivo.Length - 1) + "back"; //extensión .back    
+            archivotrad = archivo.Remove(archivo.Length - 1) + "trad"; //extensión .trad
+
+            Escribir = new StreamWriter(archivoback);
+            StreamWriter EscribirTrad = new StreamWriter(archivotrad);
+            Leer = new StreamReader(archivo);
+
+            string palabra = "";
+
+            do
+            {
+                i_caracter = Leer.Read();
+
+                if (i_caracter == -1)
+                {
+                    break; // sal del do-while, ya no proceses nada
+                }
+
+                char tipo = Tipo_caracter(i_caracter);
+
+                if (tipo == 'l') // palabra (se devolvio una letra) 
+                {
+                    palabra = "";
+                    while (char.IsLetterOrDigit((char)i_caracter) || i_caracter == '_')
+                    {
+                        palabra += (char)i_caracter;
+                        i_caracter = Leer.Read();
+                    }
+
+                    int idx = reservadas.IndexOf(palabra);
+                    if (idx != -1) // es palabra reservada
+                    {
+                        Escribir.WriteLine("palabra reservada: " + palabra);
+                        EscribirTrad.Write(traducciones[idx] + " ");
+                    }
+                    else // identificador
+                    {
+                        Escribir.WriteLine("identificador: " + palabra);
+                        EscribirTrad.Write(palabra + " ");
+                    }
+                }
+                else if (tipo == 'd') // dígito
+                {
+                    Escribir.WriteLine("digito: " + (char)i_caracter);
+                    EscribirTrad.Write((char)i_caracter);
+                }
+                else if (tipo == 's') // símbolo
+                {
+                    Escribir.WriteLine("simbolo: " + (char)i_caracter);
+                    EscribirTrad.Write((char)i_caracter);
+                }
+                else if (tipo == 'n') // salto de línea
+                {
+                    Escribir.WriteLine("salto de linea");
+                    EscribirTrad.Write("\n");
+                    N_linea++;
+                }
+                else if (tipo == 'x') //Cadena
+                {
+                    bool cerrada;
+                    string contenidoCadena = Cadena(out cerrada);
+                    Escribir.WriteLine("cadena:\" " + contenidoCadena + "\"");
+
+                    if (cerrada) //verifica que si se haya cerrado la cadena
+                        EscribirTrad.Write("\"" + contenidoCadena + "\"");
+                    else
+                        EscribirTrad.Write("\"" + contenidoCadena); // no la cierra en el .trad
+
+                }
+
+                else if (tipo == 'j')
+                {
+                    int siguiente = Leer.Read();
+                    if (siguiente == '/') // comentario de línea
+                    {
+                        string comentario = "";
+                        while (siguiente != '\n' && siguiente != -1)
+                        {
+                            comentario += (char)siguiente;
+                            siguiente = Leer.Read();
+                        }
+                        Escribir.WriteLine("comentario: " + comentario);
+                        EscribirTrad.Write("");
+                    }
+                    else if (siguiente == '*')
+                    {
+                        string comentario = "";
+                        int anterior = 0; //APUNTADOR PARA GUARDAR EL CARACTER ANTERIOR
+                        siguiente = Leer.Read();
+
+                        while (!(anterior == '*' && siguiente == '/') && siguiente != -1)
+                        {
+                            comentario += (char)siguiente;
+                            anterior = siguiente;
+                            siguiente = Leer.Read();
+                        }
+                        Escribir.WriteLine("comentario bloque: " + comentario);
+                        EscribirTrad.WriteLine("/*" + comentario + "*/");
+                    }
+
+                    else
+                    {
+                        // no era comentario, solo era '/'
+                        Escribir.WriteLine("simbolo: /");
+                        EscribirTrad.Write("/");
+                        i_caracter = siguiente; // regresamos el último caracter leído al flujo
+                    }
+                }
+
+                else if (tipo == 'g') // simbolo !
+                {
+                    Escribir.WriteLine("operador logico: " + (char)i_caracter);
+                    EscribirTrad.Write((char)i_caracter);
+
+                }
+                else if (i_caracter != -1) // cualquier otro carácter
+                {
+                    Escribir.WriteLine("otro: " + (char)i_caracter);
+                    EscribirTrad.Write((char)i_caracter);
+                }
+
+            } while (i_caracter != -1);
+
+            Rtbx_salida.AppendText("Errores: " + N_error);
+            Escribir.Close();
+            EscribirTrad.Close();
+            Leer.Close();
+        }
+
+        private void cOMPILARToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+        private char Tipo_caracter(int caracter)
+        {
+            //ASCII A-Z y de a-z
+            if ((caracter >= 65 && caracter <= 90) || (caracter >= 97 && caracter <= 122))
+            {
+                return 'l'; // Letra
+            }
+
+            else if (caracter >= 48 && caracter <= 57)
+            {
+                return 'd'; // Dígito
+            }
+
+            else if (caracter >= 60 && caracter <= 62)
+            {
+                return 'i'; // Operador relacional
+            }
+            else
+            {
+                switch (caracter)
+                {
+                    case 10: return 'n'; // ASCII salto de linea 10
+                    case 34: return 'x'; // ASCII inicio de cadena 34
+                    case 39: return 'c'; // ASCII de inicio de caracter 39
+                    case 47: return 'j'; // ASCII de barra 47
+                    //programar para los casos que sean simbolos y regresar 's'
+                    case 33: return 'g'; // ASCII de !
+                    default: return 's'; //si no es de los casos anteriores es error
+
+                }
+            }
+
+
+
+        }
+
+        //-----CORREGIR, SI DETECTA UNA "", lo asume como comentario aunque no acabe 
+        private string Cadena(out bool cerrada)
+        {
+            string contenido = "";
+            cerrada = false;
+            do
+            {
+                i_caracter = Leer.Read();
+                if (i_caracter == 10) N_linea++; //ASCI 10 = SALTO DE LINEA
+                if (i_caracter != 34 && i_caracter != -1) // ASCII fin de cadena 34 y fin de archivo -1
+                {
+                    contenido += (char)i_caracter; // acumula el carácter (se va formando la cadena)
+                }
+            } while (i_caracter != 34 && i_caracter != -1);
+
+            if (i_caracter == 34)
+            {
+                cerrada = true; // sí se cerró correctamente
+            }
+            else
+            {
+                Error(-1); // cadena sin cerrar
+            }
+
+            return contenido;
+        }
+        private void Error(int i_caracter)
+        {
+            Rtbx_salida.AppendText("Error léxico " + i_caracter + ", línea " + N_linea + "\n");
+            N_error++;
+        }
+
+
+        // Lista de palabras reservadas en C
+        List<string> reservadas = new List<string>
+        {
+            "auto","break","case","char","const","continue","default","do","double",
+            "else","enum","extern","float","for","goto","if","inline","int","long",
+            "register","restrict","return","short","signed","sizeof","static","struct",
+            "switch","typedef","union","unsigned","void","volatile","while",
+            "_Alignas","_Alignof","_Atomic","_Bool","_Complex","_Generic","_Imaginary",
+            "_Noreturn","_Static_assert","_Thread_local","asm","bool","catch","class",
+            "const_cast","delete","dynamic_cast","explicit","export","false","friend",
+            "mutable","namespace","new","operator","private","protected","public",
+            "reinterpret_cast","static_cast","template","this","throw","true","try",
+            "typeid","typename","using","virtual","wchar_t","main","include","define"
+        };
+
+        // Traducciones en español (mismo orden que la lista de arriba)
+        List<string> traducciones = new List<string>
+        {
+            "auto","romper","caso","carácter","constante","continuar","defecto","hacer","doble",
+            "sino","enumeración","externo","flotante","para","ir","si","en línea","entero","largo",
+            "registro","restringido","retornar","corto","con signo","tamaño de","estático","estructura",
+            "selección","definir tipo","unión","sin signo","vacío","volátil","mientras",
+            "alinear como","alineación de","atómico","booleano","complejo","genérico","imaginario",
+            "sin retorno","afirmación estática","hilo local","ensamblador","booleano","capturar","clase",
+            "conversión constante","eliminar","conversión dinámica","explícito","exportar","falso","amigo",
+            "mutable","espacio de nombres","nuevo","operador","privado","protegido","público",
+            "conversión reinterpretada","conversión estática","plantilla","este","lanzar","verdadero","intentar",
+            "tipoid","nombre de tipo","usando","virtual","carácter ancho","principal","incluir","definir"
+        };
+
+    } 
+}
